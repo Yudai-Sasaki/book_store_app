67f555cd18b15123dd42f6343f0c39fc22dd1871
佐々木雄大 <yudai@MacBook-Pro.local>
2020-08-08 16:10:00 +0900

    デザイン修正

--- book_store_app/src/containers/bookList.js
+++ book_store_app/src/containers/bookList.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import './home.css';
-import BookListRow from './bookListRow';
+import React from "react";
+import "./home.css";
+import BookListRow from "./bookListRow";
 
 class BookList extends React.Component {
-    render(){
-        return(
-            <table className = "bookListTable">
-                <div className = "bookListThElement">                    
-                    <tr className = "bookListRowTr">
-                        <th className = "bookListRowTh">本の題名</th>
-                    </tr>
-                </div>
-                <div className = "bookListTdElement">
-                    <BookListRow/>
-                </div>
-            </table>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="bookListThElement">
+          <table className="bookListTable">
+            <thead>
+              <tr className="bookListRowTr">
+                <th className="bookListRowTh">本の題名</th>
+                <th className="bookListRowTh">詳細</th>
+                <th className="bookListRowTh">削除</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div className="bookListTdElement">
+          <table className="bookListTable">
+            <tbody>
+              <BookListRow />
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
 export default BookList;
--- book_store_app/src/containers/bookListRow.js
+++ book_store_app/src/containers/bookListRow.js
@@ -1,49 +1,18 @@
-import React from 'react';
-import './home.css';
+import React from "react";
+import "./home.css";
 
 class BookListRow extends React.Component {
-    render(){
-        return(
-            <>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            <tr className = "bookListRowTr">
-                <td className = "bookListRowTd">教科書</td>
-            </tr>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <tr className="bookListRowTr">
+          <td className="bookListRowTd">教科書</td>
+          <td className="bookListRowTd">i</td>
+          <td className="bookListRowTd">!!</td>
+        </tr>
+      </>
+    );
+  }
 }
 
 export default BookListRow;
